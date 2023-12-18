7e526f854de3088a0f61154c4b3ab01f0b5d0cb9
cassie_c <cassie.nightingale.13.13@gmail.com>
2023-12-18 16:25:49 -0600

    course 175

--- src/App.js
+++ src/App.js
@@ -1,5 +1,19 @@
-function App() {
-  return <div className="App"></div>;
-}
+import React from "react";
 
-export default App;
+class Counter extends React.Component {
+  constructor(props) {
+    super(props);
+
+    this.state = { count: 5 };
+  }
+  render() {
+    return (
+      <div>
+        <button>-</button>
+        <span>{this.state.count}</span>
+        <button>+</button>
+      </div>
+    );
+  }
+}
+export default Counter;
